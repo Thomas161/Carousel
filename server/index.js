db2b52a5e78391c23695dbe31dd7041a0a033d18
Tom Magin <magintc@mac.com>
2019-10-23 16:05:44 +1100

    refactored files, setup express server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const port = 8080;
 
+app.use(express.static("src"));
+app.use(express.static(__dirname + "src"));
+
 app.get("/", (req, res) => {
-  res.sendFile(
-    "/Users/tommydates/Desktop/ALLREACT/htmlcarousel/src/index.html"
-  );
+  res.sendFile(path.join(__dirname, "src", "index.html"));
+});
+app.get("*", (err, req, res, next) => {
+  setImmediate(() => {
+    next(new Error("Oops something went wrong"));
+  });
+  console.log(err.message);
+  res.json({ message: err.message });
 });
 
 app.listen(port);
